f6dee5b5306484e506d1eb0d53981312711e2d78
SkiPPy43 <skippy4343@yandex.ru>
2022-05-05 19:17:03 +0300

    catch err

--- src/renderer/javascripts/components/pages/RegistrationPage/RegistrationPage.js
+++ src/renderer/javascripts/components/pages/RegistrationPage/RegistrationPage.js
@@ -4,24 +4,41 @@ function RegistrationPage() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const response = await fetch('http://localhost:3001/users/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        password: password,
-      }),
-    });
-
-    // console.log(name, email, password);
-    console.log(response);
+    try {
+      const response = await fetch('http://localhost:3001/users/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+      console.log(response.status);
+      if (response.status === 401) {
+        setError('Этот email занят');
+      }
+      if (response.status === 500) {
+        setError('Вы не заполнили все поля');
+      }
+    } catch (err) {
+      setError(err);
+      console.log('============================', err);
+      // console.log(name, email, password);
+    }
   }
+  //   import axios from "axios"
+  // export const THUNK_getRoom = (id) => async (dispatch) => {
+  //   const url = `http://localhost:3000/rooms/${id}`;
+  //   const r = await axios.get(url);
+  //   dispatch(ACTION_getRoom(r.data));
+  // };
 
   return (
     <div>
@@ -31,6 +48,7 @@ function RegistrationPage() {
         <input onChange={(e) => setPassword(e.target.value)} type="text" name="password" placeholder="password" />
         <button type="submit">Submit</button>
       </form>
+      <p>{ error }</p>
     </div>
   );
 }
