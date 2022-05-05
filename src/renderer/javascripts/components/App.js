35722c08596b698164a0ac107c55b3e966747678
SkiPPy43 <skippy4343@yandex.ru>
2022-05-05 18:05:12 +0300

    regform

--- src/renderer/javascripts/components/App.js
+++ src/renderer/javascripts/components/App.js
@@ -5,7 +5,7 @@ import NavBar from './static/header/NavBar/NavBar';
 import VideoPage from './pages/VideoPage/VideoPage';
 import GamePage from './pages/GamePage/GamePage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
-import RegistrationPage from './pages//RegistrationPage/RegistrationPage';
+import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 import Footer from './static/footer/Footer/Footer';
 import store from '../../../redux/store/store';
 
@@ -14,14 +14,14 @@ function App() {
     <div>
       <NavBar />
       <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<VideoPage />} />
-        <Route path="/video" element={<VideoPage />} />
-        <Route path="/game" element={<GamePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/registration" element={<RegistrationPage />} />
-      </Routes>
-      <Footer />
+        <Routes>
+          <Route path="/" element={<VideoPage />} />
+          <Route path="/video" element={<VideoPage />} />
+          <Route path="/game" element={<GamePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
+        </Routes>
+        <Footer />
       </Provider>
     </div>
   );
--- src/renderer/javascripts/components/pages/RegistrationPage/RegistrationPage.js
+++ src/renderer/javascripts/components/pages/RegistrationPage/RegistrationPage.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function RegistrationPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await fetch('http://localhost:3001/users/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      }),
+    });
+
+    // console.log(name, email, password);
+    console.log(response);
+  }
+
   return (
-    <div>Your code Registration page</div>
+    <div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input onChange={(e) => setName(e.target.value)} type="text" name="name" placeholder="name" />
+        <input onChange={(e) => setEmail(e.target.value)} type="text" name="email" placeholder="email" />
+        <input onChange={(e) => setPassword(e.target.value)} type="text" name="password" placeholder="password" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 export default RegistrationPage;
